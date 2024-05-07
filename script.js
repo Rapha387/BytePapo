ed25e6d2d02ae4fd6c9d9ba571f0fa6d096dd3d0
LAB53 <lab53@fatec.com>
2024-05-07 11:53:56 -0300

    continuando

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu = document.getElementById("chkMenuHeader");
+
+if(btnMenuHeader){
+    btnMenuHeader.addEventListener('click', function(){
+      btnMenuHeader.classList.toggle('animarMenu');
+    })
+  }
