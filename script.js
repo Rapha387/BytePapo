7e9e717da5d92c0195483c13efcb0f1978b38adf
Rapha387 <raphalima337@gmail.com>
2024-05-08 22:39:11 -0300

    quase finalizado

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 const menu = document.getElementById("chkMenuHeader");
 
 if(btnMenuHeader){
-    btnMenuHeader.addEventListener('click', function(){
+    btnMenuHeader.addEventListener('click', () => {
       btnMenuHeader.classList.toggle('animarMenu');
     })
   }
+
+
+  const anchors = document.querySelectorAll('a[href^="#"]');
+
+  if(anchors){
+    anchors.forEach(function(anchor) {
+        anchor.addEventListener('click', scrollToSection);
+    });
+  }
+
+  function scrollToSection(event) {
+      event.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      const offset = 75;
+      const targetPosition = target.getBoundingClientRect().top + window.scrollY - offset;
+      window.scrollTo({
+          top: targetPosition,
+          behavior: 'smooth'
+      });
+  }
+
+const btnVerMais = document.getElementById('btnVerMais')
+
+if(btnVerMais){
+  btnVerMais.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.open("https://soundcloud.com/raphalima337/sets/bytepapo", '_blank')
+  })
+}
